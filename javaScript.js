aef1c30e1ed600e4f00747d6765e0183e20ddf0b
Camilo Andres Moreno Roa <48422816+Camilo1423@users.noreply.github.com>
2022-08-19 21:06:09 -0500

    Correccion del boton copiar y ajustes del textarea

--- javaScript.js
+++ javaScript.js
@@ -1,62 +1,64 @@
-const mensaje = document.querySelector('.mensaje-original');
-const encriptado = document.querySelector('.mensaje-encriptado');
+const mensaje = document.querySelector(".mensaje-original");
+const encriptado = document.querySelector(".mensaje-encriptado");
 
 //asigno evento clic a los elementos boton
-const btnEncripta = document.getElementById('encripta');
-btnEncripta.onclick = btnEncriptar;
 
-const btnDesencripta = document.getElementById('desencripta');
-btnDesencripta.onclick = btnDesencriptar;
+// funcion encriptar
+const btnEncripta = document.getElementById("encripta");
+btnEncripta.addEventListener("click", (e) => {
+  e.preventDefault();
+  const texto = encriptar(mensaje.value);
+  encriptado.value = texto;
+});
 
-const btnCopiar = document.getElementById('btnCopia');
-btnCopiar.onclick = btnCopiando;
+// funcion desencriptar
+const btnDesencripta = document.getElementById("desencripta");
+btnDesencripta.addEventListener("click", (e) => {
+  e.preventDefault();
+  const texto = desEncriptar(mensaje.value);
+  encriptado.value = texto;
+});
 
-//funciones onclick
-function btnEncriptar () {
-    const texto = encriptar(mensaje.value);
-    encriptado.value = texto;
+// funciona para copiar el texto
+const copiarTexto = () => {
+    encriptado.select()
+    document.execCommand('copy')
+    console.log('texto copiado');
 }
 
-function btnDesencriptar () {
-    const texto = desEncriptar(mensaje.value);
-    encriptado.value = texto;
-}
-
-function btnCopiando () {
-    const texto = encriptado.value;
-    mensaje.value = texto;
-    encriptado.value = "";
-}
 
 //matriz de encriptado
-const matriz = [    ["e", "enter"], 
-                    ["i", "imes"], 
-                    ["a", "ai"], 
-                    ["o", "ober"], 
-                    ["u", "ufat"]   ];
+const matriz = [
+  ["e", "enter"],
+  ["i", "imes"],
+  ["a", "ai"],
+  ["o", "ober"],
+  ["u", "ufat"],
+];
 
 // Funciones para encriptar y desencriptar
 function encriptar(textoEncriptar) {
-    
-    textoEncriptar = textoEncriptar.toLowerCase();
+  textoEncriptar = textoEncriptar.toLowerCase();
 
-    for (let i = 0; i < matriz.length; i++) {
-        if(textoEncriptar.includes(matriz[i][0])){
-            textoEncriptar = textoEncriptar.replaceAll(matriz[i][0], matriz[i][1]);
-        }
+  for (let i = 0; i < matriz.length; i++) {
+    if (textoEncriptar.includes(matriz[i][0])) {
+      textoEncriptar = textoEncriptar.replaceAll(matriz[i][0], matriz[i][1]);
     }
-    return textoEncriptar;
+  }
+  return textoEncriptar;
 }
 
 function desEncriptar(textoDesencriptar) {
-    
-    textoDesencriptar = textoDesencriptar.toLowerCase();
+  textoDesencriptar = textoDesencriptar.toLowerCase();
 
-    for (let i = 0; i < matriz.length; i++) {
-        if (textoDesencriptar.includes(matriz[i][1])) {
-            textoDesencriptar = textoDesencriptar.replaceAll(matriz[i][1], matriz[i][0]);
-        }
+  for (let i = 0; i < matriz.length; i++) {
+    if (textoDesencriptar.includes(matriz[i][1])) {
+      textoDesencriptar = textoDesencriptar.replaceAll(
+        matriz[i][1],
+        matriz[i][0]
+      );
     }
+  }
 
-    return textoDesencriptar;
+  return textoDesencriptar;
 }
